f204c38c04995536f9a8a4ce8c3d425baa02df8b
Giorgi Kobulashvili <webdevra@bk.ru>
2021-11-20 17:12:12 +0200

    spaces

--- helpers.js
+++ helpers.js
@@ -1,11 +1,11 @@
 const axios = require('axios')
 
 const request = async (url) => {
-	return axios.get(url).then(res => res.data).catch(err => {
-		console.error(err)
-	})
+  return axios.get(url).then(res => res.data).catch(err => {
+    console.error(err)
+  })
 }
 
 module.exports = {
-    request
+  request
 }
