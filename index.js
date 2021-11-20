268809ff22526668900b2b76b3a5af745cca4caf
Giorgi Kobulashvili <webdevra@bk.ru>
2021-11-20 17:11:49 +0200

    spaces

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
 const listener = require('./listener')
-const { request } = require('./helpers')
+const {
+  request
+} = require('./helpers')
 
 
-const { ASSETS_FROM, ASSETS_TO }  = process.env
+const {
+  ASSETS_FROM,
+  ASSETS_TO
+} = process.env
 
 const ASSET_CATEGORY = 'BNB'
-const filterAssets = ({ q }) => q === ASSET_CATEGORY 
+const filterAssets = ({
+  q
+}) => q === ASSET_CATEGORY
 
 const getAssets = async () => {
-	const URL = "https://www.binance.com/bapi/asset/v2/public/asset-service/product/get-products?includeEtf=true"
-	const { data } = await request(URL)
-	return data.filter(filterAssets)
+  const URL = "https://www.binance.com/bapi/asset/v2/public/asset-service/product/get-products?includeEtf=true"
+  const {
+    data
+  } = await request(URL)
+  return data.filter(filterAssets)
 }
 
 const main = async () => {
-	const assets = await getAssets()
+  const assets = await getAssets()
 
-	for(asset of assets.slice(ASSETS_FROM, ASSETS_TO)) {
-		const { s: symbol } = asset
-		listener(symbol)
-	}
+  for (asset of assets.slice(ASSETS_FROM, ASSETS_TO)) {
+    const {
+      s: symbol
+    } = asset
+    listener(symbol)
+  }
 }
 
 (async () => {
-	await main()
+  await main()
 })()
